16a03fe6bd8252c708fbc8795b0371bc5c996994
Adrian Tadeo <adntd24@gmail.com>
2025-10-29 15:03:17 -0700

    feat: Modelo general de producto actualizado

---
+++ BackEnd/Migrations/20251029215438_ImgUrl.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImgUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                columns: table => new
+                {
+                    BrandId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.BrandId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BrandId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Brands_BrandId",
+                        column: x => x.BrandId,
+                        principalTable: "Brands",
+                        principalColumn: "BrandId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    FormFactorSupport = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MaxGpuLengthMM = table.Column<int>(type: "int", nullable: false),
+                    MaxCoolerHeightMM = table.Column<int>(type: "int", nullable: false),
+                    PsuFormFactor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DriveBays = table.Column<int>(type: "int", nullable: false),
+                    HasGlassPanel = table.Column<bool>(type: "bit", nullable: false),
+                    CaseType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rgb = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cases", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cases_Products_Id",
+                        column: x => x.Id,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CpuCoolers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TDP = table.Column<int>(type: "int", nullable: false),
+                    FanSize = table.Column<int>(type: "int", nullable: false),
+                    Fancount = table.Column<int>(type: "int", nullable: false),
+                    Rgb = table.Column<bool>(type: "bit", nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: false),
+                    MaxRpm = table.Column<int>(type: "int", nullable: false),
+                    NoiseLevel = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CpuCoolers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CpuCoolers_Products_Id",
+                        column: x => x.Id,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Graficas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Chipset = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VRam = table.Column<int>(type: "int", nullable: false),
+                    MemoryType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TDP = table.Column<int>(type: "int", nullable: false),
+                    PCIeVersion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Warranty = table.Column<int>(type: "int", nullable: false),
+                    Lenght = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Graficas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Graficas_Products_Id",
+                        column: x => x.Id,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Motherboards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Socket = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Chipset = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MaxRamGB = table.Column<int>(type: "int", nullable: false),
+                    FormFactor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RamSlots = table.Column<int>(type: "int", nullable: false),
+                    HasWiFi = table.Column<bool>(type: "bit", nullable: false),
+                    SupportedMemType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Motherboards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Motherboards_Products_Id",
+                        column: x => x.Id,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PowerSupplies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Modular = table.Column<bool>(type: "bit", nullable: false),
+                    Certidication = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FormFactor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Wattage = table.Column<int>(type: "int", nullable: false),
+                    Rgb = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PowerSupplies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PowerSupplies_Products_Id",
+                        column: x => x.Id,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Processors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cores = table.Column<int>(type: "int", nullable: false),
+                    Threads = table.Column<int>(type: "int", nullable: false),
+                    BaseClockGHz = table.Column<float>(type: "real", nullable: false),
+                    BoostClockGHz = table.Column<float>(type: "real", nullable: false),
+                    SocketType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TDP = table.Column<int>(type: "int", nullable: false),
+                    IntegratedGraphics = table.Column<bool>(type: "bit", nullable: false),
+                    Architecture = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Lithography = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Processors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Processors_Products_Id",
+                        column: x => x.Id,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Rgb = table.Column<bool>(type: "bit", nullable: false),
+                    CapacityGB = table.Column<int>(type: "int", nullable: false),
+                    SpeedMTs = table.Column<int>(type: "int", nullable: false),
+                    MemType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rams", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rams_Products_Id",
+                        column: x => x.Id,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Storages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Interface = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CapacityGB = table.Column<int>(type: "int", nullable: false),
+                    ReadSpeedMBps = table.Column<int>(type: "int", nullable: false),
+                    WriteSpeedMBps = table.Column<int>(type: "int", nullable: false),
+                    FormFactor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Warranty = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Storages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Storages_Products_Id",
+                        column: x => x.Id,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_BrandId",
+                table: "Products",
+                column: "BrandId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cases");
+
+            migrationBuilder.DropTable(
+                name: "CpuCoolers");
+
+            migrationBuilder.DropTable(
+                name: "Graficas");
+
+            migrationBuilder.DropTable(
+                name: "Motherboards");
+
+            migrationBuilder.DropTable(
+                name: "PowerSupplies");
+
+            migrationBuilder.DropTable(
+                name: "Processors");
+
+            migrationBuilder.DropTable(
+                name: "ProductCategories");
+
+            migrationBuilder.DropTable(
+                name: "Rams");
+
+            migrationBuilder.DropTable(
+                name: "Storages");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Brands");
+        }
+    }
+}
--- BackEnd/Models/Product.cs
+++ BackEnd/Models/Product.cs
@@ -20,4 +20,6 @@ public class Product
     public int Stock { get; set; }
 
     public decimal Price { get; set; }
+
+    public string ImageUrl { get; set; }
 }
